a98b8dfe37c2adc166998564670077c859ebc947
JoytikaJ <joytika@gmail.com>
2016-08-28 13:05:14 -0700

    whitespace

--- stars.js
+++ stars.js
@@ -10,9 +10,4 @@ $(document).ready(function() {
   $('.saturn-bottom').css({'transform':'rotateZ(-10deg)'});
 //animations for planets orbiting the sun
 //timing taken from http://www.sjsu.edu/faculty/watkins/orbital.htm
-
-
-
-
 }); //ending jquery dom init
-
